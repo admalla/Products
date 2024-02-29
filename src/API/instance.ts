56befa7e473b18afd21bcaf75afb1e8f01cb36e5
admalla <maasovec@gmail.com>
2024-02-29 11:19:08 +0300

    some fixed

--- src/API/instance.ts
+++ src/API/instance.ts
@@ -3,7 +3,7 @@ import { generateXAuth } from "../util";
 
 
 export const instance = axios.create({
-  baseURL: "https://cors-anywhere.herokuapp.com/http://api.valantis.store:40000/",
+  baseURL: "https://crossorigin.me/http://api.valantis.store:40000/",
   headers: {
     'X-Auth': generateXAuth('Valantis'),
   }
